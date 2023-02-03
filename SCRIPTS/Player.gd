7b37dcd9e795c0c26f6cc80f0a49405564b1b815
MimStar <80539513+MimStar@users.noreply.github.com>
2023-02-03 23:13:05 +0100

    Player v0.1

---
+++ SCRIPTS/Player.gd
@@ -0,0 +1,15 @@
+extends Spatial
+
+var look_rot = Vector3.ZERO
+var sensitivity = 0.1
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _physics_process(delta):
+	rotation_degrees.y = look_rot.y * sensitivity
+	translate(Vector3(0,0,-0.1) * delta)
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		look_rot.y -= event.relative.x
