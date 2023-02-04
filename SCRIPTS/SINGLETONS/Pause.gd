6cd7d281fca648f857dc10b37b8f65ac384b73aa
Skar0ps <swayllesg.sg@gmail.com>
2023-02-04 20:38:54 +0100

    Update menu principal

--- SCRIPTS/SINGLETONS/Pause.gd
+++ SCRIPTS/SINGLETONS/Pause.gd
@@ -8,6 +8,7 @@ onready var cover = $Cover
 func _ready():
 	connect("unpaused",get_tree(),"set",["paused",false])
 	connect("paused",get_tree(),"set",["paused",true])
+	$FocusCursor.set_process(false)
 
 func _input(event):
 	if event.is_action_pressed("ui_pause"):
@@ -16,10 +17,13 @@ func _input(event):
 func pause():
 	emit_signal("paused")
 	print("has been paused")
+	$FocusCursor.set_process(true)
 
 func unpause():
 	emit_signal("unpaused")
 	print("HAS BEEN UNPAUSED")
+	$FocusCursor.set_process(false)
+	
 
 func toggle_pause():
 	unpause() if get_tree().paused else pause()
