5361a58003c60ccf76b5986115d9fe99ed80e9b2
MimStar <80539513+MimStar@users.noreply.github.com>
2023-02-04 12:18:27 +0100

    Trail of the roots

--- SCRIPTS/Player.gd
+++ SCRIPTS/Player.gd
@@ -1,14 +1,14 @@
 extends Spatial
 
 var look_rot = Vector3.ZERO
-var sensitivity = 0.1
+var sensitivity = 0.01
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func _physics_process(delta):
 	rotation_degrees.y = look_rot.y * sensitivity
-	translate(Vector3(0,0,-0.1) * delta)
+	translate(Vector3(0,0,-5) * delta)
 	
 func _input(event):
 	if event is InputEventMouseMotion:
